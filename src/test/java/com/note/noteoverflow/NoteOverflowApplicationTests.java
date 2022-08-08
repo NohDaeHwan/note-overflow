655ecac59946adc052c06fe83c9fe33b10521830
NohDaeHwan <ndh5953@naver.com>
2022-08-08 16:00:18 +0900

    #4 - Start spring boot project
    스프링 부트 프로젝트 초기화
    
    들어간 Dependencies:
    
    * Spring Web
    * Spring Boot DevTools
    * Lombok
    * Validation
    * JSTL
    * Tomcat-Embed-Jasper

---
+++ src/test/java/com/note/noteoverflow/NoteOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.noteoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteOverflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
