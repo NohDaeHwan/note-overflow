954c1008f768c0d87240c99b8416912a829f640a
NohDaeHwan <ndh5953@naver.com>
2022-08-16 21:59:33 +0900

    #12 - Create a tag-related view test
    어떤 경로로 뷰를 열어줄지 테스트로 명세 작성

---
+++ src/main/java/com/note/noteoverflow/controller/NoteTagsController.java
@@ -0,0 +1,9 @@
+package com.note.noteoverflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/tags")
+@Controller
+public class NoteTagsController {
+}
