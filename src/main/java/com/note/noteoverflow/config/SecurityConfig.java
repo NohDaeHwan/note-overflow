45de43955f2f62a4d2486daee38417f3b142edeb
NohDaeHwan <ndh5953@naver.com>
2022-09-23 23:27:48 +0900

    #17 - security config
    시큐리티 설정을 위한 기본 세팅

---
+++ src/main/java/com/note/noteoverflow/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.note.noteoverflow.config;
+
+import com.note.noteoverflow.dto.UserAccountDto;
+import com.note.noteoverflow.dto.security.NotePrincipal;
+import com.note.noteoverflow.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable().authorizeHttpRequests(auth -> auth
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .antMatchers("/notes/create").authenticated()
+                .anyRequest().permitAll()
+                )
+                .formLogin()
+                    .loginPage("/login").and()
+                .logout()
+                    .logoutSuccessUrl("/").and()
+                .build();
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findByUserEmail(username)
+                .map(UserAccountDto::from)
+                .map(NotePrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/com/note/noteoverflow/dto/security/NotePrincipal.java
@@ -0,0 +1,99 @@
+package com.note.noteoverflow.dto.security;
+
+import com.note.noteoverflow.domain.type.RoleType;
+import com.note.noteoverflow.dto.UserAccountDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record NotePrincipal(
+        Long id,
+        String userEmail,
+        String userPassword,
+        String nickname,
+        String userPhone,
+        RoleType userRole,
+        int sharedCount
+) implements UserDetails {
+
+    public static NotePrincipal of(Long id, String userEmail, String userPassword, String nickname, String userPhone, RoleType userRole, int sharedCount) {
+        return new NotePrincipal(
+                id,
+                userEmail,
+                userPassword,
+                nickname,
+                userPhone,
+                userRole,
+                sharedCount
+        );
+    }
+
+    public static NotePrincipal from(UserAccountDto dto) {
+        return NotePrincipal.of(
+                dto.id(),
+                dto.userEmail(),
+                dto.userPassword(),
+                dto.nickname(),
+                dto.userPhone(),
+                dto.userRole(),
+                dto.sharedCount()
+        );
+    }
+
+    public UserAccountDto toDto() {
+        return UserAccountDto.of(
+                id,
+                userEmail,
+                userPassword,
+                nickname,
+                userPhone,
+                userRole,
+                sharedCount
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<RoleType> roleTypes = Set.of(userRole);
+
+        return roleTypes.stream()
+                .map(RoleType::getName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Override
+    public String getPassword() {
+        return userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return userEmail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
