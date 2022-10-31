64a3e7bdb5c17957bcce2702521f2f187b15ba57
NohDaeHwan <ndh5953@naver.com>
2022-10-31 10:30:37 +0900

    #25 - 알림 컨트롤러 구현
    모든 Url에 알림을 보여주기 위해서 View에 데이터를 보냄
    좋아요, 팔로우 기능을 구현하기 위해 컨트롤러 구현

--- src/main/java/com/note/noteoverflow/controller/NoteController.java
+++ src/main/java/com/note/noteoverflow/controller/NoteController.java
@@ -1,13 +1,16 @@
 package com.note.noteoverflow.controller;
 
 import com.note.noteoverflow.dto.request.NoteRequest;
+import com.note.noteoverflow.dto.response.NotificationResponse;
 import com.note.noteoverflow.dto.security.NotePrincipal;
 import com.note.noteoverflow.service.NoteService;
+import com.note.noteoverflow.service.SharedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -16,8 +19,19 @@ public class NoteController {
 
     private final NoteService noteService;
 
+    private final SharedService sharedService;
+
     @GetMapping("/notes/create")
-    public String noteCreate() {
+    public String noteCreate(@AuthenticationPrincipal NotePrincipal principal,
+                             ModelMap map) {
+        Long loginId = 0L;
+        if (principal != null) {
+            loginId = principal.id();
+        }
+
+        NotificationResponse notificationResponse = sharedService.getNotification(loginId);
+
+        map.addAttribute("notificationResponse", notificationResponse);
         return "notes/create";
     }
 
@@ -26,8 +40,7 @@ public class NoteController {
     @PostMapping("/save_request")
     public ResponseEntity<Integer> saveRequest(@RequestBody NoteRequest noteRequest,
                                                @AuthenticationPrincipal NotePrincipal principal) {
-        System.out.println(noteRequest);
-        int result = noteService.saveRequest(noteRequest.toDto(principal.toDto()));
+        int result = noteService.saveRequest(noteRequest, principal.toDto());
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
--- src/main/java/com/note/noteoverflow/controller/NoteTagsController.java
+++ src/main/java/com/note/noteoverflow/controller/NoteTagsController.java
@@ -1,13 +1,16 @@
 package com.note.noteoverflow.controller;
 
 import com.note.noteoverflow.dto.TagListDto;
+import com.note.noteoverflow.dto.response.NotificationResponse;
+import com.note.noteoverflow.dto.security.NotePrincipal;
 import com.note.noteoverflow.service.NoteTagsService;
 import com.note.noteoverflow.service.PaginationService;
-import com.querydsl.core.Tuple;
+import com.note.noteoverflow.service.SharedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,16 +28,26 @@ public class NoteTagsController {
 
     private final PaginationService paginationService;
 
+    private final SharedService sharedService;
+
     @GetMapping
     public String tags(@RequestParam(defaultValue = "") String tag,
                        @PageableDefault(size = 10) Pageable pageable,
+                       @AuthenticationPrincipal NotePrincipal principal,
                        ModelMap map
     ) {
+        Long loginId = 0L;
+        if (principal != null) {
+            loginId = principal.id();
+        }
+
         Page<TagListDto> tagList = noteTagsService.selectTagList(tag, pageable);
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), tagList.getTotalPages());
-        System.out.println(barNumbers);
+        NotificationResponse notificationResponse = sharedService.getNotification(loginId);
+
         map.addAttribute("paginationBarNumbers", barNumbers);
         map.addAttribute("tags", tagList);
+        map.addAttribute("notificationResponse", notificationResponse);
         return "tags/tags";
     }
 
