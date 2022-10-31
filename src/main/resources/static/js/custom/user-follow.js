1394f2df596b8cd33bc77bc659f6e83add848561
NohDaeHwan <ndh5953@naver.com>
2022-10-31 10:28:00 +0900

    #25 - follow javascript
    팔로우 기능을 수행하는 자바스크립트

---
+++ src/main/resources/static/js/custom/user-follow.js
@@ -0,0 +1,39 @@
+function followAdd(userId) {
+
+    $.ajax({
+        type: "POST",
+        url: "/user-follow/add/"+userId,
+        contentType: "application/json; charset=utf-8",
+    }).done(function(response) {
+        console.log(response);
+        if (response == -1) {
+            location.href="/login";
+        } else {
+            $("#user-follow").html("<span>"+response+"명</span>" +
+                "<button class='btn btn-danger btn-sm' th:onclick='followDelete("+userId+")'>팔로우 취소</button>");
+        }
+    }).fail(function(error) {
+        alert(JSON.stringify(error));
+    });
+
+}
+
+function followDelete(userId) {
+
+    $.ajax({
+        type: "POST",
+        url: "/user-follow/delete/"+userId,
+        contentType: "application/json; charset=utf-8",
+    }).done(function(response) {
+        console.log(response);
+        if (response == -1) {
+            location.href="/login";
+        } else {
+            $("#user-follow").html("<span>"+response+"명</span>" +
+                "<button class='btn btn-warning btn-sm' th:onclick='followAdd("+userId+")'>팔로우</button>");
+        }
+    }).fail(function(error) {
+        alert(JSON.stringify(error));
+    });
+
+}
